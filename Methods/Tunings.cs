8db3ec02b7bac6db19adf00a077dbaa8d4087ab5
Jack <=>
2017-06-16 17:28:22 +0100

    Feature Adding
    -added save method
    -added add song UI
    -added tunings class

---
+++ Methods/Tunings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Methods
+{
+    public static class Tunings
+    {
+        public static BindingList<string> ListOfTunings;
+
+        public static 
+    }
+}
